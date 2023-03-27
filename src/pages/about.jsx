719ff0742eb3cdbf6a996c74eac2cca99c181f32
jose031199 <luisking531@gmail.com>
2023-03-26 18:36:29 -0700

    Added sass file to style it

---
+++ src/pages/about.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>
+      <h2>This is about the about page</h2>
+      <p>Example im nervous</p>
+    </div>
+  )
+}
+
+
+export const Head = ()=> <title>About Page</title>
+export default about
---
+++ src/pages/index.jsx
@@ -0,0 +1,15 @@
+import * as React from "react"
+import  '../styles/style.scss'
+
+const IndexPage = () => {
+  return (
+    <main className="main">
+      <h1>Welcome to my Gatsby site!</h1>
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </main>
+  )
+}
+
+
+export const Head = () => <title>Home Page</title>
+export default IndexPage
